9018fb319673cd6e50a8a5be551b3d30e33573cb
munnamihir <munnamihir1999@gmail.com>
2023-02-06 15:56:26 -0500

    second assignment Forth commit

--- answer.py
+++ answer.py
@@ -37,7 +37,7 @@ def strings():
 
     # Replace "great" with "good" in variable great and assign it to a new variable good.
 
-    good = stevens[0:11]+" good"
+    good = stevens[0:12]+"good"
 
     return stevens, stevens_7, length, great, good
 
